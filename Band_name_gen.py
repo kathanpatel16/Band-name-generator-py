4b0581dcd8e7c71854f0c13effed137e2ff09038
kathanpatel16 <140890678+kathanpatel16@users.noreply.github.com>
2023-07-29 16:32:13 +0530

    Project complete

---
+++ Band_name_gen.py
@@ -0,0 +1,3 @@
+city_name=input("Which city they grew up\n")
+pet_name=input("what is yore pet name\n")
+print("you grow up in " + city_name + "and your pet name is " + pet_name + " we love your band")
